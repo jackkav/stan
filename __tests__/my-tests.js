9a1247f7fd9f3948daba97d269e3842048aa3e02
Jack Kavanagh <jackkav@gmail.com>
2018-03-16 14:10:14 +0800

    12 hour

--- __tests__/my-tests.js
+++ __tests__/my-tests.js
@@ -38,3 +38,23 @@ test('sort numbers and that', () => {
 function bigSorting (arr) {
   return arr.sort((a, b) => +b <= +a)
 }
+
+test('12 hour dates and that', () => {
+  expect(timeConversion('07:45:00PM')).toEqual('19:45:00')
+  expect(timeConversion('12:45:00PM')).toEqual('12:45:00')
+  expect(timeConversion('12:45:00AM')).toEqual('00:45:00')
+})
+
+function timeConversion (s) {
+  const midday = '12'
+  const midnight = '00'
+  const isAM = s[8] !== 'P'
+  const isTwelve = s.slice(0, 2) === '12'
+  const isMidday = !isAM && isTwelve
+  const isMidnight = isAM && isTwelve
+  const hours = +s.slice(0, 2)
+  const minutesAndSeconds = s.slice(2, 8)
+  if (isMidday) return midday + minutesAndSeconds
+  if (isMidnight) return midnight + minutesAndSeconds
+  return isAM ? s.slice(0, 8) : hours + 12 + minutesAndSeconds
+}
